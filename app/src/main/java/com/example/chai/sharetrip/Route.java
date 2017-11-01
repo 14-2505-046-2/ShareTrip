50e781474b25903baa67af079904c3dd79633314
14-2505-046-2 <u046ff@yamaguchi-u.ac.jp>
2017-11-01 16:58:29 +0900

    データベースの定義のみ。完成。

---
+++ app/src/main/java/com/example/chai/sharetrip/Route.java
@@ -0,0 +1,25 @@
+package com.example.chai.sharetrip;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by chai on 2017/11/01.
+ */
+
+public class Route extends RealmObject {
+    @PrimaryKey
+    //最低限必要なもの
+    public long root_id;
+    public long tour_id;
+    public boolean flag_area;//場所ならTrueで交通手段ならFalseでお願いします。
+    public String comment;
+    //場所の場合必要なもの（交通手段なら参照されない。）
+    public String name;
+    public byte[] image;
+    public String link;
+    //交通手段で必要なもの（場所なら参照されない。）
+    public int means;//この交通手段は0-6までで（0:徒歩 1:自転車 2:車 3:バス 4:電車 5:新幹線 6:飛行機
+    public String start_time;
+    public String end_time;
+}
---
+++ app/src/main/java/com/example/chai/sharetrip/Tour.java
@@ -0,0 +1,23 @@
+package com.example.chai.sharetrip;
+
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by chai on 2017/11/01.
+ */
+
+//ツアーの一覧画面用のデータベース。
+//tour_idはツアーの詳細画面用のRootデータベースのtour_idと同じになる様に。
+public class Tour extends RealmObject {
+    @PrimaryKey
+    public long tour_id;
+    public String tour_title;
+    public String start_time;
+    public String total_time;
+    public String upload_date;
+    public String author;
+    public String comment;
+    public byte[] image;
+}

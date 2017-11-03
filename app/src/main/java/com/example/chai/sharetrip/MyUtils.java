7da4b711730f9d58d185c5ac5c8f8ecb5db41867
14-2505-046-2 <u046ff@yamaguchi-u.ac.jp>
2017-11-03 12:01:27 +0900

    画像の表示、保存に使用するMyUtilsクラスの作成。

---
+++ app/src/main/java/com/example/chai/sharetrip/MyUtils.java
@@ -0,0 +1,63 @@
+package com.example.chai.sharetrip;
+
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by chai on 2017/11/03.
+ */
+
+public class MyUtils {
+
+    //byte型配列からBitmapインスタンスへの変換　-> p314
+    public static Bitmap getImageFromByte(byte[] bytes) {
+        BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inJustDecodeBounds = true;
+        BitmapFactory.decodeByteArray(bytes, 0, bytes.length, opt);
+        int bitmapSize = 1;
+
+        if((opt.outHeight * opt.outWidth) > 5000) {
+            double outSize = (double) (opt.outHeight * opt.outWidth) / 500000;
+            bitmapSize = (int)(Math.sqrt(outSize) + 1);
+        }
+        opt.inJustDecodeBounds = false;
+        opt.inSampleSize = bitmapSize;
+        Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, opt);
+        return bmp;
+    }
+    public static byte[] getByteFromImage(Bitmap bmp) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
+    }
+
+    //Bitmapインスタンスからbyte配列への変換 -> p314
+    public static Bitmap getImageFromStream(ContentResolver resolver, Uri uri)
+            throws IOException {
+        InputStream in;
+        BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inJustDecodeBounds = true;
+        in = resolver.openInputStream(uri);
+        BitmapFactory.decodeStream(in, null, opt);
+        in.close();
+        int bitmapSize = 1;
+        if((opt.outHeight * opt.outWidth) > 500000) {
+            double outSize = (double) (opt.outHeight * opt.outWidth) / 500000;
+            bitmapSize = (int)(Math.sqrt(outSize) + 1);
+        }
+
+        opt.inJustDecodeBounds = false;
+        opt.inSampleSize = bitmapSize;
+        in = resolver.openInputStream(uri);
+        Bitmap bmp = BitmapFactory.decodeStream(in,null,opt);
+        in.close();
+        return bmp;
+    }
+}

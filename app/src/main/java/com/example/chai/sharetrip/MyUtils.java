47d30997e847f606bf6040710c5f9262c546b69f
14-2505-046-2 <u046ff@yamaguchi-u.ac.jp>
2017-12-04 02:42:11 +0900

    ルートの削除ボタンを作成しました。

--- app/src/main/java/com/example/chai/sharetrip/MyUtils.java
+++ app/src/main/java/com/example/chai/sharetrip/MyUtils.java
@@ -29,6 +29,7 @@ import io.realm.RealmResults;
 public class MyUtils {
     public static ArrayList<Long> list = new ArrayList<Long>();
     public static boolean is_test = true;//テストの間はtrueで。（upload用)
+    public static final long ADDROUTE = -1;
 
     //byte型配列からBitmapインスタンスへの変換　-> p314
     public static Bitmap getImageFromByte(byte[] bytes) {
@@ -107,7 +108,7 @@ public class MyUtils {
         mRealm.beginTransaction();
         mRealm = Realm.getDefaultInstance();
         Number maxId = mRealm.where(Tour.class).max("tour_id");
-        long nextId = 1;
+        long nextId = 0;
         if (maxId != null) nextId = maxId.longValue() + 1;
         Tour tour = mRealm.createObject(Tour.class, new Long(nextId));
         tour.tour_title = o.getString("tour_title");
@@ -329,5 +330,25 @@ public class MyUtils {
         } catch (NCMBException e) {
             Log.e("delete_test", e.toString());
         }
+    }
+
+    public static void reload_add() {
+        final Realm mRealm = Realm.getDefaultInstance();
+        mRealm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                if (!mRealm.where(Route.class).equalTo("route_id", ADDROUTE).findAll().isEmpty()) {
+                    Route delete_route = realm.where(Route.class).equalTo("route_id", ADDROUTE).findFirst();
+                    delete_route.deleteFromRealm();
+                }
+            }
+        });
+        mRealm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Route route = realm.createObject(Route.class, ADDROUTE);
+                Log.d("add_button", String.valueOf(ADDROUTE));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/chai/sharetrip/TripDetailFragment.java
+++ app/src/main/java/com/example/chai/sharetrip/TripDetailFragment.java
@@ -19,7 +19,6 @@ public class TripDetailFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
     private Realm mRealm;
-    public static final long ADDROUTE = -1;
 
     public TripDetailFragment() {
         // Required empty public constructor
@@ -56,28 +55,13 @@ public class TripDetailFragment extends Fragment {
         Boolean local_flag = false;
         if(tour.objectId.equals("local_data")) {
             local_flag = true;
-            mRealm.executeTransaction(new Realm.Transaction() {
-                  @Override
-                  public void execute(Realm realm) {
-                      if (!mRealm.where(Route.class).equalTo("route_id", ADDROUTE).findAll().isEmpty()) {
-                          Route delete_route = realm.where(Route.class).equalTo("route_id", TripDetailFragment.ADDROUTE).findFirst();
-                          delete_route.deleteFromRealm();
-                      }
-                  }
-            });
-            mRealm.executeTransaction(new Realm.Transaction() {
-                @Override
-                public void execute(Realm realm) {
-                    Route route = realm.createObject(Route.class, ADDROUTE);
-                    Log.d("add_button", String.valueOf(ADDROUTE));
-                }
-            });
+            MyUtils.reload_add();
         }
 
         RealmQuery query = mRealm.where(Route.class).equalTo("tour_id", DetailActivity.tour_id);
         if(local_flag) {
             query = query.or();
-            query = query.equalTo("route_id", ADDROUTE);
+            query = query.equalTo("route_id", MyUtils.ADDROUTE);
         }
         RealmResults<Route> routes = query.findAll();
 

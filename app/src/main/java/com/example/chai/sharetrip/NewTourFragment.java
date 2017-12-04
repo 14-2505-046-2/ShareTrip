cc1b7d76a2c2198cce0895c9b57d25d39a63b453
enPiT-P22 <g902ff@yamaguchi.ac.jp>
2017-12-04 16:39:47 +0900

    12/04　新規ツアーの名前が入力されるようになりました。

--- app/src/main/java/com/example/chai/sharetrip/NewTourFragment.java
+++ app/src/main/java/com/example/chai/sharetrip/NewTourFragment.java
@@ -3,6 +3,9 @@ package com.example.chai.sharetrip;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,7 @@ import android.widget.EditText;
 
 import io.realm.Realm;
 
+//教科書p305～を参考にしています。
 public class NewTourFragment extends Fragment {
 
     private static final String TOUR_ID =  "TOUR_ID";
@@ -50,7 +54,36 @@ public class NewTourFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_new_tour,container, false);
 
-        return inflater.inflate(R.layout.fragment_new_tour, container, false);
+        mTitleEdit = (EditText) v.findViewById(R.id.editTitle);
+        mTitleEdit.setText("abc");
+        Log.d("title","log");
+
+        mTitleEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(final Editable s) {
+                mRealm.executeTransactionAsync(new Realm.Transaction() {
+                    @Override
+                    public void execute (Realm realm) {
+                        Tour tour = realm.where(Tour.class).equalTo("tour_id", mTourId).findFirst();
+                        tour.tour_title = mTitleEdit.getText().toString();
+                        Log.d("tourid","" + mTourId);
+                        Log.d("title","" + mTitleEdit.getText());
+                    }
+                });
+            }
+        });
+
+        return v;
     }
 
     @Override
